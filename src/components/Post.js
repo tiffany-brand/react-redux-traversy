362f6b02d659ab2c8f5c694aad2f64ed5a406c20
Tiffany Brand <brand2learn@gmail.com>
2020-12-28 21:07:44 -0500

    Add redux store functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
-
-componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => this.setState({posts: data}))
+	render() {
+		const postItems = this.props.posts.map((post) => (
+			<div key={post.id}>
+				<h3>{post.title}</h3>
+				<p>{post.body}</p>
+			</div>
+		));
+		return (
+			<div>
+				<h1>Posts</h1>
+				{postItems}
+			</div>
+		);
+	}
 }
 
-    render() {
-        const postItems = this.state.posts.map(post => (
-            <div key={post.id}>
-                <h3>{post.title}</h3>
-                <p>{post.body}</p>
-            </div>
-        )) 
-        return (
-            <div>
-                <h1>Posts</h1>
-                { postItems }
-            </div>
-        )
-    }
-}
+Posts.propTypes = {
+	fetchPosts: PropTypes.func.isRequired,
+	posts: PropTypes.array.isRequired
+};
 
-export default Posts;
+const mapStateToProps = (state) => ({
+	posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
